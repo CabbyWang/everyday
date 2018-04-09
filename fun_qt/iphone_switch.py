03c69d44f48097069b075ba49517be15106de460
wangsiyong <18489969713@163.com>
2018-04-10 00:16:30 +0800

    buttongroup互斥时全不选的解决办法(稍微麻烦点, 不知道是不是有更好的方法)

--- fun_qt/iphone_switch.py
+++ fun_qt/iphone_switch.py
@@ -17,15 +17,32 @@ class IphoneSwitch(QDialog):
         # Exclusive
         for i in range(3):
             checkbox = QCheckBox('switch{}'.format(i), self)
+            checkbox.pressed.connect(self.pressed)
+            checkbox.released.connect(self.released)
             # button = QPushButton('button{}'.format(i), self)
             layout.addWidget(checkbox)
             # layout.addWidget(button)
             button_group.addButton(checkbox)
             # button_group.addButton(button)
-        print(button_group.exclusive())
+        # print(button_group.exclusive())
         layout.setSpacing(20)
         layout.setContentsMargins(10, 10, 10, 10)
         self.setLayout(layout)
+
+    def pressed(self):
+        self.checkboxes = {}
+        checkbox = self.sender()
+        print('pressed ', checkbox.checkState())
+        self.checkboxes[checkbox] = checkbox.checkState()
+
+    def released(self):
+        checkbox = self.sender()
+        state = checkbox.checkState()
+        print('released', state)
+        if state == self.checkboxes.get(checkbox, 0):   # 都为2, 说明状态没有改变
+            self.button_group.setExclusive(False)
+            checkbox.setChecked(False)
+            self.button_group.setExclusive(True)
 
 
 if __name__ == '__main__':
